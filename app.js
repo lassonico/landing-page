8f02dfac49bd9fea28d923dc96d60ea71e9e147e
lassonico <sionvmx@gmail.com>
2023-08-24 08:36:46 -0500

    Justes de animaciones

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    animacionesi()
+    animacionesd()
+})
+
+const textoi = document.querySelector('.textos');
+const textod = document.querySelector('.textos-derecha');
 const boton = document.querySelector('.btn');
 const mensaje = "Hola, estoy interesado. *¿Me pueden dar más información de los créditos?*"
 const telefono = "573232858951";
@@ -6,3 +13,17 @@ const url = "https://wa.me/" + telefono + "?text=" + encodeURIComponent(mensaje)
 boton.addEventListener('click', () => {
     window.open(url, "_blank");
 })
+
+function animacionesi(){
+    setTimeout(() => {
+        textoi.classList.remove('texthidden')
+        textoi.classList.add('animaciontexti')
+    }, 2800)
+}
+
+function animacionesd(){
+    setTimeout(() => {
+        textod.classList.remove('texthidden')
+        textod.classList.add('animaciontextd')
+    }, 4000);
+}
